a55d80c1d837cfd51ef146a69dac65373533749e
Rempacious <rempacious@gmail.com>
2023-11-30 02:56:35 +0800

    ADD

--- Bookworm.java
+++ Bookworm.java
@@ -1,29 +1,56 @@
-import javax.swing.JFrame;
-import javax.swing.JTextField;
-
+import java.util.Scanner;
 public class Bookworm {
-    public static void main(String[] args) {
-        // Create a JFrame
-        JFrame frame = new JFrame("Bookworm Game");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        public static void main(String[] args) throws InterruptedException {
+            //Seperator para mas maganda yung output. System.out.println(seperator); lang para magamit
+            String seperator = "---------------------------------------";
+            
+            //Start ng laro dito
+            //Name input and welcome message
+            Scanner inputName = new Scanner(System.in);
+            System.out.println("Welcome to ByteBeacon: A Bookworm Game!");
+            Thread.sleep(1000);
+            System.out.println(seperator);
+            System.out.println("Please enter your name: ");
+            String name = inputName.nextLine();
+            System.out.println(seperator);
+            System.out.println("Welcome " + name + "!");
 
-        // Create a JTextField
-        JTextField textField = new JTextField();
-        textField.setBounds(300, 250, 200, 30); // Set the position and size of the text field
+            //Wait for 1 second bago i-execute yung next line
+              Thread.sleep(1000);
+            
+            //Game instructions
+            System.out.println("The goal of the game is type in the longest word you can find in the given set of random letters.");
+                Thread.sleep(2000);
+                System.out.println(seperator);
+            System.out.println("The longer the word, the more damage you do to the enemy.");
+                Thread.sleep(2000);
+                System.out.println(seperator);
+            System.out.println("You have 30 seconds to type in the word everytime it's your turn.");
+                Thread.sleep(2000);
+                System.out.println(seperator);
 
-        // Add the text field to the frame
-        frame.add(textField);
+            Scanner input = new Scanner(System.in);
+            System.out.println("Have you understood the instructions? (Y/N)");
+            String answer = input.nextLine();
+            if (answer.equals("Y")) {
+                System.out.println(seperator);
+                System.out.println("Let's start the game!");
+            } else if (answer.equals("N")) {
+                System.out.println(seperator);
+                System.out.println("Please read the instructions again.");
+            } else {
+                System.out.println(seperator);
+                System.out.println("Please enter Y or N only.");
+            }
 
-        // Set the size of the frame>
-        frame.setSize(800, 600);
 
-        // Set the layout to null
-        frame.setLayout(null);
-
-        // Center the frame on the screen
-        frame.setLocationRelativeTo(null);
-
-        // Make the frame visible
-        frame.setVisible(true);
+    
+            /* TODO: 
+            •	Create a simple enemy of the game with HP bar. (10%)
+            •	Insert input words from the user that will be used as the weapon -- Haggang dito muna yung game. (30% na to ng game.)
+            •	Random words will be generated in which the user should type the longest word they could find from it. (50%)
+            •	Enemy HP bar will be depleted depending on the word length (80%)
+            •	The player will win if the enemy HP bar is depleted. (100%)
+            */
+        }
     }
-}
